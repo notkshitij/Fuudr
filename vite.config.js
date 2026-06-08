67c5f9630832be8a993576344397f33e0895946e
NEGO2522 <borbreak@gmail.com>
2026-06-08 14:55:18 +0530

    fix: force cache busting with hashed asset filenames

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -12,6 +11,9 @@ export default defineConfig({
             return 'vendor';
           }
         },
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
       },
     },
   },
